9392c19c922f7a674030676595bc77ee884260aa
sam <sam@163.com>
2020-08-29 10:52:52 +0800

    http middleware cors

--- http/middleware/cors.go
+++ http/middleware/cors.go
@@ -2,13 +2,24 @@ package middleware
 
 import (
    "github.com/gin-gonic/gin"
+   "net/http"
 )
 
 // CrossDomain 全局添加跨域允许
 func Cors() gin.HandlerFunc {
    return func(c *gin.Context) {
-      c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-      c.Writer.Header().Set("Access-Control-Allow-Headers", "access-token, x-requested-with, content-type")
-      c.Writer.Header().Set("Access-Control-Allow-Methods","POST, GET, OPTIONS, PUT, PATCH, DELETE")
+      method := c.Request.Method
+      c.Header("Access-Control-Allow-Origin", "*")
+      c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+      c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
+      c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+      c.Header("Access-Control-Allow-Credentials", "true")
+
+      // 放行所有OPTIONS方法，因为有的模板是要请求两次的
+      if method == "OPTIONS" {
+         c.AbortWithStatus(http.StatusNoContent)
+      }
+      // 处理请求
+      c.Next()
    }
 }
