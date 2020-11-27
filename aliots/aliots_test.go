bde25c1b4fe877ef9df4a45bd4eb36514ea16512
sam <sam@163.com>
2020-11-27 16:53:01 +0800

    ots test

--- aliots/aliots_test.go
+++ aliots/aliots_test.go
@@ -5,35 +5,22 @@ import "github.com/moka-mrp/sword-core/config"
 var conf config.OtsMultiConfig
 func init()  {
 	conf=make(config.OtsMultiConfig,2)
-	//conf["tulong"]=config.OtsConfig{
-	//	EndPoint:        "",
-	//	InstanceName:    "",
-	//	AccessKeyId:     "",
-	//	AccessKeySecret: "",
-	//}
-	//
-	//
-	//conf["xueyin"]=config.OtsConfig{
-	//	EndPoint:        "",
-	//	InstanceName:    "",
-	//	AccessKeyId:     "",
-	//	AccessKeySecret: "",
-	//}
-
 	conf["tulong"]=config.OtsConfig{
-		EndPoint:        "https://tulong.cn-shanghai.ots.aliyuncs.com",
-		InstanceName:    "tulong",
-		AccessKeyId:     "LTAI4GG1DPRi2yty32FqBdj3",
-		AccessKeySecret: "FUpx9HHvJ2SUiSuVk6OaI4SehsGM3A",
+		EndPoint:        "",
+		InstanceName:    "",
+		AccessKeyId:     "",
+		AccessKeySecret: "",
 	}
 
 
 	conf["xueyin"]=config.OtsConfig{
-		EndPoint:        "https://xueyin.cn-shanghai.ots.aliyuncs.com",
-		InstanceName:    "xueyin",
-		AccessKeyId:     "LTAI4GG1DPRi2yty32FqBdj3",
-		AccessKeySecret: "FUpx9HHvJ2SUiSuVk6OaI4SehsGM3A",
+		EndPoint:        "",
+		InstanceName:    "",
+		AccessKeyId:     "",
+		AccessKeySecret: "",
 	}
+
+
 
 
 
